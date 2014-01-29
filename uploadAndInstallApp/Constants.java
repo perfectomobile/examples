d9c36f34f1a935c1c7aab4b23b22d8ea138709fb
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-01-29 10:26:16 -0500

    Create Constants.java

---
+++ uploadAndInstallApp/Constants.java
@@ -0,0 +1,15 @@
+//  apk or ipa file location on the local machine
+public static final String WORK_LIB = "C:\\api\\";
+
+// Device ID
+	public static final String DEVICE = "XXXXXX";
+// application name 
+	public static final String APP_NAME = "bofa.ipa";
+	
+//REPOSITORY_KEY in perfecto mobile system 	
+	public static final String REPOSITORY_KEY = "PRIVATE:temp/"+APP_NAME;
+
+// Perfecto Mobile cloud details
+	public static final String PM_USER = "****@perfectomobile.com";
+	public static final String PM_PASSWORD = "*********";
+	public static final String PM_CLOUD = "*****.perfectomobile.com";
