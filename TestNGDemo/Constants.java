69ef91b785bbb24b1fb46eeef9292145c3ce34b7
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-02-26 18:08:49 -0500

    Create Constants.java

---
+++ TestNGDemo/Constants.java
@@ -0,0 +1,11 @@
+public class Constants
+{
+  /** Project Constants */
+   
+   public static final String PM_USER = "USER";
+   public static final String PM_PASSWORD = "Password";
+   public static final String PM_CLOUD = "cloud.perfectomobile.com";
+     
+	public static final String REPORT_LIB = "C:\\Test\\";
+
+   }
