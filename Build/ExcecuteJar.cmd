5b3283058cac0a451fb96318c876d6ea12bc3d63
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-08-15 16:55:51 -0400

    Update ExcecuteJar.cmd

--- Build/ExcecuteJar.cmd
+++ Build/ExcecuteJar.cmd
@@ -1,2 +1,2 @@
 echo "start Build"
-java -jar Build.jar "3152168C4EAA1E2A5DE93CF7B89222720E3A62E0" "c:\\Perfectomobile\\CIDemo"
+java -jar C:\Perfectomobile\CIDemo\Build.jar "3152168C4EAA1E2A5DE93CF7B89222720E3A62E0" "c:\\Perfectomobile\\CIDemo"
