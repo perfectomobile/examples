e3cae0842fd7ef4d70a3955547e76c80076c08f1
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-08-29 09:56:13 -0400

    Update ExcecuteJar.cmd

--- Build/ExcecuteJar.cmd
+++ Build/ExcecuteJar.cmd
@@ -1,2 +1,5 @@
 echo "start Build"
 java -jar C:\Perfectomobile\CIDemo\Build.jar "3152168C4EAA1E2A5DE93CF7B89222720E3A62E0" "c:\\Perfectomobile\\CIDemo"
+
+
+java -jar C:\Perfectomobile\CIDemo\JenkinsSeleniumDemo_v1.2.jar DEVICE_ID=XXXXXXXX BUILD_LOCATION=C:\\Perfectomobile\\CIDemo REPOSITORY_KEY=PRIVATE:Apps APP_NAME=PMIOSDemo.ipa
