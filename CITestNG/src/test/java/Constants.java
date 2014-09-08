b5cb110c68a04dfcaf3b3335aa818f794d914ce7
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-09-08 15:35:41 -0400

    Create Constants.java

---
+++ CITestNG/src/test/java/Constants.java
@@ -0,0 +1,30 @@
+package test.java;
+public class Constants
+{
+	/** Project Constants */
+	public static final String WORK_LIB = "C:\\Jenkins_ex\\";
+	//public static final String WORK_LIB = "C:\\api\\";
+
+	// iphone 7 public 
+	//public static final String DEVICE = "85C3C17D957CCC302C81F78DDE59C4F71DFF1AAE";
+	// iphone 6 public
+	//public static final String DEVICE = "1AF1270E544117EBC3E3D4B5AC27A9E34E5F6B54";
+
+ 	public static final String DEVICE = "4859ABC4B4F7EF8CCD9FF0C4F8A47B9FFC0B5D0A";
+	
+
+//	public static final String APP_NAME = "Southwest.ipa";
+	public static final String APP_NAME = "PMIOSDemo.ipa";
+	public static final String REPOSITORY_KEY = "PRIVATE:uziHTTPTest/"+APP_NAME;
+
+//	public static final String PM_USER = "uzie@perfectomobile.com";
+	public static final String PM_PASSWORD = "Perfecto1";
+	public static final String PM_USER = "uzie@perfectomobile.com";
+
+	public static final String PM_CLOUD = "demo.perfectomobile.com";
+	
+	public static final String REPORT_LIB = "c:\\test\\";
+
+}
+
+
